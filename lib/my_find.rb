6b5562f5c42c0ca71bd7eb264675d813aacf1fae
Jesse Wenger <wengerj@gmail.com>
2021-03-03 20:55:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  while < collection.length 
+  while i < collection.length 
     return collection[i] if 
       yield(collection[i])
     i = i + 1 
